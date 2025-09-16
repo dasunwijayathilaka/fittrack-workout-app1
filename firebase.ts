d5a85ee0b5ea977508220912dece45f4ced6a41d
Dasun Wijayathilaka <maxdasun613@gmail.com>
2025-09-16 09:01:12 +0530

    new update

--- firebase.ts
+++ firebase.ts
@@ -7,9 +7,17 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAq5LPUTtfLoLTaVzUjHiC0qJ3nXOAMs8s",
+  authDomain: "fittrack-workout-app.firebaseapp.com",
+  projectId: "fittrack-workout-app",
+  storageBucket: "fittrack-workout-app.firebasestorage.app",
+  messagingSenderId: "1020522724550",
+  appId: "1:1020522724550:web:2eedd7d525473465572bc7",
+};
 
 // Initialize Firebase
-//const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-//export const auth = getAuth(app);
-//export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
